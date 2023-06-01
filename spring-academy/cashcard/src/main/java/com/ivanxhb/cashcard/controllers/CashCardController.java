bfb1bed75f6f0847e999116eb79eebe8334b0d2d
ivanhb <10326824+ivanxhb@users.noreply.github.com>
2023-06-01 14:45:31 -0500

    updates2

---
+++ spring-academy/cashcard/src/main/java/com/ivanxhb/cashcard/controllers/CashCardController.java
@@ -0,0 +1,30 @@
+package com.ivanxhb.cashcard.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ivanxhb.cashcard.CashCard;
+
+@RestController // Tells Spring that this is a RestController component
+@RequestMapping("/cashcards") // Tells Spring that instances of this class is the endpoint to  HTTP requests to /cashcard.
+
+public class CashCardController {
+	
+	@GetMapping("/{requestedID}") // @GetMapping handles HTTP GET requests. /{requestedID} signals that we Respond to those requests
+	public ResponseEntity<CashCard> findById(@PathVariable Long requestedID) { // Realize that the return type is a ResponseEntity, that is a CashCard objects
+
+        // Explain: what @PathVariable does.
+        
+        if(requestedID.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        }
+        else{
+            return ResponseEntity.notFound().build(); // why is build required?
+        }
+
+	}
+}
--- spring-academy/cashcard/src/test/java/com/ivanxhb/cashcard/CashcardApplicationTests.java
+++ spring-academy/cashcard/src/test/java/com/ivanxhb/cashcard/CashcardApplicationTests.java
@@ -1,13 +1,71 @@
 package com.ivanxhb.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+ * Updated during Lab: implementing GET
 @SpringBootTest
 class CashcardApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+}
+*/
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CashCardApplicationTests {
+	
+	/*
+	 * What does @Autowired do?
+	 * 
+	 * A: We tell Spring to "inject" a test helper that'll allow us to make HTTP request to the locally running app.
+	 * 
+	 * @Autowired is a form of Spring dependecy injection it's best used only in tests
+	 * 
+	 */
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+    	/*
+    	 * Look more into restTemplate and TestRestTemplate.
+    	 * And the ResponseEntity Class.
+    	 */
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class); // Why is the responseType String here? If in the backend this URI is returning a CashCard type?
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		Number id = documentContext.read("$.id");
+		//assertThat(id).isNotNull(); // Test 1
+		assertThat(id).isEqualTo(99); // Test 2
+
+		Double amount = documentContext.read("$.amount");
+		assertThat(amount).isEqualTo(123.45);
+	
+	}
+
+	@Test
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
+
+
+
 
 }
